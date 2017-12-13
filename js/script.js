58974dfe3ce7fb9de18132ae9fe4763085ff419a
yurialeksndr <yuriatp@gmail.com>
2017-12-13 21:49:04 -0200

    Implementa a lógica de criação dinâmica da tabela de acordo com o conteúdo.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 var info_shown = false;
 
+var data = {
+	exemplo01 : {
+		nome : "MDF (Medium Density Fiberboard)",
+		permitido : "no",
+		justificativa : "A máquina de corte a laser é um ferramenta rica em possibilidades criativas, mas também requer cuidados particulares."
+	},
+
+	exemplo02 : {
+		nome : "COMPENSADO",
+		permitido : "yes",
+		justificativa : "A máquina de corte a laser é um ferramenta rica em possibilidades criativas, mas também requer cuidados particulares."
+	}
+}
+
 $(document).on('click', '.justification', function() {
 	info_shown = true;
 
@@ -29,3 +43,48 @@ $(document).on('click', '#bg_justification', function() {
 	}
 
 });
+
+$(document).ready(function(){
+  criaTabela();
+  criaCabecalhoTabela();
+  criaCelulasTabela();
+});
+
+function criaTabela() {
+	$("#infotable").append("<div class='divTable'></div>");
+	$(".divTable").append("<div class='divTableBody'></div>");
+}
+
+function criaCabecalhoTabela() {
+	$(".divTableBody").append("<div id='header' class='divTableRow'></div>");
+	$("#header").append("<div class='divTableCell divTableHeading column01'>Material</div>");
+	$("#header").append("<div class='divTableCell divTableHeading column02'>Pode cortar?</div>");
+	$("#header").append("<div class='divTableCell divTableHeading column03'>Por quê?</div>");
+}
+
+function criaCelulasTabela() {
+	how_many_keys = Object.keys(data).length;
+	counter = 1;
+
+	for (var key in data) {
+		$(".divTableBody").append("<div id='row" + counter + "' class='divTableRow'></div>");
+
+		$("#row" + counter).append("<div class='divTableCell column01'>" + data[key].nome + "</div>");
+		$("#row" + counter).append("<div class='divTableCell column02 " + data[key].permitido + "'>" + loadPermission(data[key].permitido) + "</div>");
+		$("#row" + counter).append("<div class='divTableCell column03 justification'>+</div>");
+
+		counter = counter + 1;
+	}
+}
+
+function loadPermission(key) {
+	if (key == "yes") {
+		return "SIM"
+	}
+	else if (key == "no") {
+		return "NÃO"
+	}
+	else {
+		return "SEM INFO"
+	}
+}
